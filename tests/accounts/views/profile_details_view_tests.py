d4cc1ca0b1634f72c05cbe16354e98d814117c2d
dilyaniliev7 <dilyaniliev7@abv.bg>
2024-06-20 16:33:05 +0300

    Sign up / profile-details Tests added

--- tests/accounts/views/profile_details_view_tests.py
+++ tests/accounts/views/profile_details_view_tests.py
@@ -12,6 +12,10 @@ class ProfileDetailsViewTests(TestCase):
         'email': 'testuser',
         'password': '12345qew',
     }
+    VALID_USER_CREDENTIALS_2 = {
+        'email': 'user2',
+        'password': 'password123'
+    }
     VALID_PROFILE_DATA = {
         'first_name': 'Test',
         'last_name': 'User',
@@ -36,3 +40,27 @@ class ProfileDetailsViewTests(TestCase):
             'pk': profile.pk,
         }))
         self.assertTemplateUsed('profile/profile-details-page.html')
+
+    def test_when_user_is_owner__expect_is_owner_to_be_true(self):
+        user = UserModel.objects.create_user(**self.VALID_USER_CREDENTIALS)
+        profile = Profile.objects.create(
+            **self.VALID_PROFILE_DATA,
+            user=user,
+        )
+        self.client.login(**self.VALID_USER_CREDENTIALS)
+        response = self.client.get(reverse('details user', kwargs={'pk': profile.pk}))
+
+        self.assertTrue(response.context['is_owner'])
+
+    def test_when_user_is_not_owner__expect_is_owner_to_be_false(self):
+        user = UserModel.objects.create_user(**self.VALID_USER_CREDENTIALS)
+        profile = Profile.objects.create(
+            **self.VALID_PROFILE_DATA,
+            user=user,
+        )
+
+        another_user = UserModel.objects.create_user(**self.VALID_USER_CREDENTIALS_2)
+        self.client.login(**self.VALID_USER_CREDENTIALS_2)
+        response = self.client.get(reverse('details user', kwargs={'pk': profile.pk}))
+
+        self.assertFalse(response.context['is_owner'])
